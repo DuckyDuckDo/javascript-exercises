e2a5cf94edd890902f6e73db8823f49a457f4702
David <daviddo2205@gmail.com>
2024-08-19 23:11:55 -0400

    Finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanString = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
+
+    let l = 0;
+    let r = cleanString.length - 1;
+
+    while (l < r){
+        if (cleanString[l] != cleanString[r]){
+            return false
+        }
+        l += 1;
+        r -= 1;
+    }
+    return true;
 };
 
 // Do not edit below this line
