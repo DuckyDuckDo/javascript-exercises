f4f1882936738361cfc4dedb611f6ae1d32bddb9
David <daviddo2205@gmail.com>
2024-08-19 23:23:48 -0400

    Finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let x = Number(number);
+    if (x < 0){
+        return 'OOPS'
+    }
+
+    if (x <= 1){
+        return x
+    }
+
+    let first = 0
+    let second = 1
+
+    for (let i = 2; i <= x; i++){
+        let tmp = second;
+        second = first + second;
+        first = tmp;
+    }
+
+    return second
+
+
+
 
 };
 
