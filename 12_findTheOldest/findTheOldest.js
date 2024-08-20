2602808b5b971788ec0632e1fb0cfebb15e8c1dd
David <daviddo2205@gmail.com>
2024-08-19 23:45:20 -0400

    finished find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const getAge = function(death, birth){
+    if (!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+
+
+const findTheOldest = function(people) {
+    const result = people.reduce((oldest, curr) => {
+        const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        const age = getAge(curr.yearOfDeath, curr.yearOfBirth);
+        return oldestAge < age ? curr : oldest;
+    });
+
+    return result;
+
+
 
 };
 
